08a6da868ff827d8257301dca091fcb4521caabf
michael <michael@icenet.nl>
2021-02-26 14:06:05 +0100

    update shizzle

--- .conf/blocklistform/contact.php
+++ .conf/blocklistform/contact.php
@@ -1,12 +1,16 @@
 <?php
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-define('DB_SERVER', 'mysql');
+define('DB_SERVER', getenv('DB_SERVER'));
 define('MYSQL_USER', getenv('MYSQL_USER'));
 define('MYSQL_PASSWORD', getenv('MYSQL_PASSWORD')); 
 define('MYSQL_DATABASE', getenv('MYSQL_DATABASE'));
 
-$con = mysqli_connect(DB_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE) or die('Failed to connect to the database, died with error:');
+$con = new mysqli(DB_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
+}
 
 
 // get the post records
@@ -17,12 +21,13 @@ $txtIpadress = $_POST['txtIpadress'];
 $txtCallnr = $_POST['txtCallnr'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_blocklist` (`Id`, `fldNaam`, `fldEmail`, `fldTelefoon`, `fldIpadress`, `fldCallnr`) VALUES ('0', '$txtNaam', '$txtEmail', '$txtTelefoon', '$txtIpadress', '$txtCallnr')";
+$sql = "INSERT INTO tbl_blocklist (Id, fldNaam, fldEmail, fldTelefoon, fldIpadress, fldCallnr) VALUES ('0', '$txtNaam', '$txtEmail', '$txtTelefoon', '$txtIpadress', '$txtCallnr')";
 
-// insert in database 
-$rs = mysqli_query($con, $sql);
-if($rs)
-{
-	echo "Contact Records Inserted";
+if ($con->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $con->error;
 }
+
+$con->close();
 ?>
